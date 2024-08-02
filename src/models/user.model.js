645c884c0d4f656d074de5f42c4d8bbf4b8fb5ae
tapan77777 <72134688+tapan77777@users.noreply.github.com>
2024-08-02 15:41:07 +0530

    add multer

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
             trim: true,
             
         },
-        fullname : {
+        fullName : {
             type : String,
             required: true,
             trim: true,
@@ -68,7 +68,7 @@ userSchema.methods.generateAccessToken = function(){
         _id: this._id,
         email: this.email,
         username: this.username,
-        fullName : this.fullname,
+        fullName : this.fullName,
     },
     process.env.ACCESS_TOKEN_SECRET,
     {
@@ -83,7 +83,7 @@ userSchema.methods.generateRefreshToken = function(){
     },
     process.env.REFRESH_TOKEN_SECRET,
     {
-        expiresIn: process.env.REFERESH_TOKEN_EXPIRY
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
     }
 )
 }
